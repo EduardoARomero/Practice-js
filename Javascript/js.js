ec1b295444367faa521fc3a7a434a88022723212
jessie <jhesy.92@gmail.com>
2022-07-09 13:47:46 -0300

    Exercises

---
+++ Javascript/js.js
@@ -0,0 +1,137 @@
+// Actividad 1:
+
+// let name = "Homero";
+// let lastName = "Simpson";
+// let age = 39;
+
+// Actividad 2:
+
+
+// const cliveland = "cliveland"
+// const sprinfield = "Sprinfield"
+// const cordobaCap = "Cordoba capital"
+// const philadelphia = "Filadelfia"
+// const norway = "Oslo"
+
+
+// Actividad 3:
+
+// let nombre = "Bart";
+// let apellido = "Simpson";
+// let direccion = "742 Av siempre verde";
+// let pais = "Sprinfield, Usa";
+// let nacimiento = "02-11-70";
+// let sexo = "masculino";
+
+// let carnet ="Nombre: "+nombre+" "+apellido+" "+
+//             "Pais: "+pais+ " "+
+//             "Direccion: "+direccion+" "+
+//             "Nacimiento: "+nacimiento+" "+
+//             "Sexo: "+sexo+" ";
+
+// console.log(carnet);
+
+// let nombre1 = prompt("ingrese Persona N1: ");
+// let nombre2 = prompt("Ingrese Persona N2: ");
+// let nombre3 = prompt("Ingrese Persona N3: ");
+// let nombre4 = prompt("Ingrese Persona N4: ");
+// let nombre5 = prompt("Ingrese Persona N5: ");
+
+
+// Actividad 4:
+
+// let entradas="primera persona: " +nombre1+" "+
+//             "Segunda persona: "+nombre2+" "+
+//             "Tercera persona: "+nombre3+" "+ 
+//             "Cuarta persona: "+nombre4+" "+ 
+//             "Quintacence people: "+nombre5;
+
+// console.log(entradas);
+
+
+// Actividad 5:
+
+// let precioUsuario = parseInt(prompt("Ingrese un precio: "));
+
+// let descuento20 = precioUsuario - 20;
+// let descuento50 = precioUsuario - 50;
+
+
+// console.log(descuento20);
+// console.log(descuento50);
+
+
+// Actividad B1:
+
+// let nombreUsuario = prompt("Ingrese un nombre: ");
+// let nombre = "Marta".toUpperCase();
+
+//         if (nombreUsuario == nombre) {
+//             alert("Fui yo");
+//         }else {
+//             alert("Yo no fui");
+//         }
+
+
+// Actividad B2:
+
+// let usuario = prompt("Ingrese una letra: ");
+
+//     if ((usuario == "Y") || (usuario == "y")){
+//         alert("CORRRREECTO");
+//     }else{
+//         alert("INCORRECTO");
+//     }
+
+
+// Actividad B3:
+
+// let gamer = prompt("Ingrese un numero del 1 al 4: ");
+
+
+// if (gamer == 1){
+//     alert("elegiste a Homero");
+// }
+
+// else if (gamer == 2){
+//     alert("Elegiste a Marge");
+// }
+
+// else if(gamer == 3){
+//     alert("Elegiste a Bart");
+// }
+// else if (gamer == 4){
+//     alert("Elegiste a Lisa");
+
+// } else{
+//     alert("Numero incorrecto!");
+// }
+
+
+// Actividad B4:
+
+// let producto1 = prompt("ingrese 1er producto:");
+// let producto2 = prompt("ingrese 2do producto:");
+// let producto3 = prompt("ingrese 3ero producto:");
+// let producto4 = prompt("ingrese 4to producto:");
+
+// if ((producto1 != "") && (producto2 != "") && (producto3 !="") && (producto4 !="")){
+
+//     let carritodecompra = "1 " +producto1+ " "+
+//                         "2 "+ producto2+ " "+
+//                         "3 "+ producto3+" "+
+//                         "4 "+producto4;
+//     console.log(carritodecompra);
+// } else{
+//     alert("Carga incorrecta");
+// }
+
+
+// Actividad C1:
+
+// let repetidor = parseInt(prompt("ingresar numero de repeticiones: "));
+// let texto = prompt("Ingresar texto a repetir: ");
+
+// for (let i=0; i < repetidor; i++){
+//     console.log(texto +" ");
+// } 
